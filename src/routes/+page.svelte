261c733a7df9845f2ee9f0068b800942a6107eca
Fernando Maclen <fmaclen@gmail.com>
2024-07-16 16:09:47 -0400

    add a placeholder image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<enhanced:img src="$lib/dummy.jpg?w=160" alt="Dummy image" />
